f5d2042a7256cd751a5e4eec33251827bcdab325
Nick <nicholasconnors@hotmail.ca>
2023-04-16 21:22:49 -0400

    Revert "added a button to generate a whole random tree of spirals"
    This reverts commit 2e9e3546fab662eb946d5c21901f091fe707a637.

--- Assets/Editor/NomaiTextArcEditor.cs
+++ Assets/Editor/NomaiTextArcEditor.cs
@@ -1,6 +1,5 @@
 using UnityEngine;
 using UnityEditor;
-using static NomaiTextArcBuilder;
 
 public class NomaiTextArcEditor : EditorWindow
 {
@@ -15,23 +14,6 @@ public class NomaiTextArcEditor : EditorWindow
         if (GUILayout.Button("Place Arc"))
         {
 			NomaiTextArcBuilder.Place();
-        }
-        
-        if (GUILayout.Button("Place Random Conversation"))
-        {
-			var root = NomaiTextArcBuilder.Place().GetComponent<NomaiTextArcBuilder.SpiralManipulator>();
-            generateChildren(root);
-        }
-    }
-
-    private void generateChildren(NomaiTextArcBuilder.SpiralManipulator root, int depth = 0) {
-        if (depth > 2) return;
-        else if (depth > 0 && Random.value < 0.5f) return;
-
-        for(var i = 0; i < Random.Range(1, 3); i++) 
-        {
-            var child = root.AddChild();
-            generateChildren(child, depth+1);
         }
     }
 }
